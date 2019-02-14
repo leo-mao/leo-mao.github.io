9137f525d1825a6306f108bd64ca591c1b3a0bdc
leo-mao <emicool.my@gmail.com>
2019-02-14 15:15:18 +0800

    fix replacing-url.sh

--- replace-url.sh
+++ replace-url.sh
@@ -7,5 +7,5 @@ if [ ! -f ./config.toml ]; then
 fi
 
 # replacing url to $1
-echo -e "URL Replacing\n"
+echo -e "\033[0;32mURL Replacing\n\033[0m"
 sed -i "s|^baseURL = .*\"|baseURL = \"$1\"|" config.toml
